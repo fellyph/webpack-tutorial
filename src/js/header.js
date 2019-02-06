76e2699689ca7006a493ba90477f1c08376334b7
fellyph <fellyph.cintra@gmail.com>
2019-02-06 12:28:40 +0000

    adding style file

--- src/js/header.js
+++ src/js/header.js
@@ -1,6 +1,2 @@
 var navigationElement = document.querySelector('.navigation');
 console.log(navigationElement);
-
-navigationElement.style.backgroundColor = 'azure';
-navigationElement.style.padding = '1em';
-
